cbdc30ed9cfb5c646464e6b19f9650d871a1c0e9
John Bourassa <johink@gmail.com>
2016-10-14 23:17:31 -0400

    Almost done with tweets

--- Week3/twitter_mining.py
+++ Week3/twitter_mining.py
@@ -110,3 +110,99 @@ for tweet in all_data:
 mydf = pd.DataFrame(summary)
 
 #%%
+import json
+#Let's dig into a more meaty dataset:  All tweets containing "#debate" during the second presidential debate
+all_data = []
+#Read in the file
+with open("c:/users/john/desktop/python course/tweets.txt") as file:
+    for line in file:
+        #Only read in the lines that are not blank
+        if not line.isspace():
+            all_data.append(json.loads(line))
+            
+print(all_data[0])
+
+#%%
+#Now we have a list containing all of the tweets; let's do some analysis!
+#What are the most common hashtags?
+#First, let's implement this ourselves:
+list_of_hashtags = []
+for tweet in all_data:
+    list_of_hashtags.extend(tweet["hashtags"])
+    
+#%%    
+#Here we will make a list of tuples, with the first item being the hashtag, and the second item being the number of times that hashtag is in the list
+counts = [(hashtag,list_of_hashtags.count(hashtag)) for hashtag in set(list_of_hashtags) ]
+
+#%%
+#Uh oh!  That was wayyyyy too slow.  Luckily, Python has many libraries with efficient implementations for common use cases
+#The Counter function does exactly what we need:
+from collections import Counter
+counts = Counter(list_of_hashtags)
+counts.most_common(15)
+
+#%%
+#Here we can see a minor issue:  It's treating #debate and #Debate as different hashtags
+#Let's redo the analysis with all the hashtags converted to lower-case
+list_of_hashtags = [x.lower() for x in list_of_hashtags]
+counts = Counter(list_of_hashtags)
+counts.most_common(15)
+
+#%%
+#Let's start analyzing the tweets themselves.  The tweets have a bunch of unnecessary junk in them,
+#so let's write a function which will clean up a tweet
+#This function will need to use regular expressions to get rid of unnecessary characters
+#as well as the Natural Language Tool-Kit (NLTK) to get rid of stop words
+#Use nltk.download() to open the download manager, go to Corpora -> stopwords and download
+import nltk
+nltk.download()
+
+#%%
+import re
+from nltk.corpus import stopwords
+import html
+
+def clean_tweet(tweet, trash):  
+    #Turn the tweet into a list of lower case individual words
+    tweet = html.unescape(tweet)
+    tweet = tweet.lower().split()
+    #Get rid of the words which are hyperlinks, and replace everything that isn't a letter, # or @ with a blank
+    tweet = [re.sub(r"[^A-z|0-9|#|@|:|/]+","", word) for word in tweet if not word.startswith("http")]
+    #Remove all the trash words
+    tweet = [word for word in tweet if word not in trash and len(word) > 2]
+    #Put all the words back together with a blank in between them
+    return " ".join(tweet)
+
+#%%
+#Make a list of words to remove  
+stop = set(stopwords.words("english"))
+
+print("Before: {}\n\n".format(all_data[12]["text"]))
+print("After: {}".format(clean_tweet(all_data[12]["text"], stop)))
+
+#%%
+#Now we'll convert all the texts
+processed_tweets = []
+for tweet in all_data:
+    processed_tweets.append(clean_tweet(tweet["text"], stop))
+    
+#%%
+#Now we can use scikit-learn's CountVectorizer to create models!
+from sklearn.feature_extraction.text import CountVectorizer
+
+#Here we create an instance of a CountVectorizer called "vectorizer"
+vectorizer = CountVectorizer(analyzer = "word", max_features = 5000)
+
+#Now we will create a Bag of Words!
+#The 5,000 most common words are given their own column, and each row represents a tweet
+#Whatever words were present in the tweet will be represented by a "1"
+train = vectorizer.fit_transform(processed_tweets)
+
+#Here we are converting the fit_transform output to a numpy array
+#numpy arrays are very efficient
+train = train.toarray()
+
+#If you want to see the words which correspond to the columns, use get_feature_names()
+vectorizer.get_feature_names()
+
+tweet = all_data[12]["text"]
