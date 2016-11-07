5a1e785ddde116b3435016a5800b4e947befb2ee
John Bourassa <johink@gmail.com>
2016-11-07 09:01:02 -0500

    Some updates to lin-alg

--- Week4/linear-algebra.py
+++ Week4/linear-algebra.py
@@ -1,3 +1,20 @@
 """LINEAR ALGEBRA"""
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.misc import imread
 
-from sklearn
+#Read in the file with RGB encoding
+pic = imread("Week4/hello.data", mode = "RGB")
+
+#It's a numpy array!
+type(pic)
+
+#.shape tells us the dimensions.  Note this is a 3d array.
+pic.shape
+
+#matplotlib.pyplot.imshow() is a convenient way to show a picture
+plt.imshow(pic)
+#%%
+#Transpose x and y axis:
+plt.imshow(np.transpose(pic,[1,0,2]))
+
